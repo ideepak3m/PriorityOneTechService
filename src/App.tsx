466edde6770c905be343a6bada18e5dc1ebc7d6b
ideepak3m <ivdeepak@gmail.com>
2025-09-27 10:49:53 -0400

    Changed Open Graph and Twitter meta tags to PriorityOne

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/PriorityOneTechService">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
